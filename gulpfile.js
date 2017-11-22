93ecb9dc57033583aab28c7c9f9f8b4ee7a4bd3d
Jan Rembold <jan.rembold@virtual-identity.com>
2017-11-22 09:21:30 +0100

    refactored to use new hbs-build-framework repo

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,1 @@
-const requireDir = require('require-dir');
-
-// require all gulp packages recursively
-requireDir('./node_modules/gulp-build-framework/gulp', { recurse: true });
+require('hbs-build-framework');
---
+++ projectConfig.js
@@ -0,0 +1,34 @@
+module.exports = {
+	project: 'VIGulpFrontendBoilerplate',
+	global: {
+		resources: [
+			'/resources'
+		],
+		tasks: {
+			cleanCss: true,
+			cssStats: true,
+			sass: true,
+			less: true,
+			favicons: true,
+			handlebars: true,
+			iconfont: true,
+			image: true,
+			linting: true,
+			markdown: false,
+			uglify: true,
+			webpack: true
+		},
+		externalResources: {
+			'vi-css-helper': ['helper.css', 'print.css'],
+			'jQuery.DebouncedResize': 'js/jquery.debouncedresize.js',
+			'media-match': 'media.match.js',
+			'conditional-resource-loader': 'src/resourceLoader.js',
+			'jquery': 'dist/jquery.js',
+			'jquery-migrate': 'dist/jquery-migrate.js',
+			'enquire': 'dist/enquire.js',
+			'jquery-debouncedwidth': 'js/jquery.debouncedwidth.js',
+			'normalize.css': 'normalize.css',
+			'handlebars': 'dist/handlebars.runtime.js'
+		}
+	}
+};
