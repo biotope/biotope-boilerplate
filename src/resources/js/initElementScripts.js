ca359ff36e9ad374d1f9c3f4e65a0d80384cf41c
SheepFromHeaven <marcwissler@gmail.com>
2018-02-16 14:36:44 +0100

    Update data init - also for non conditional resources

---
+++ src/resources/js/initElementScripts.js
@@ -0,0 +1,14 @@
+(function ($, window, document, undefined) {
+	'use strict';
+
+	ffglobal.configuration.set('initElementFunctions', function () {
+		var $elementsWithDataInit = $('[data-init]');
+		$elementsWithDataInit.each(function() {
+			if ($(this).data('init')) {
+				var initFunction = eval($(this).attr('data-init')); // jshint ignore:line
+				initFunction($(this));
+			}
+		});
+	});
+
+})(jQuery, window, document);
