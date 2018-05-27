f8580b6210e4dedf0aa6144ea4b86c3568d241f6
janrembold <janrembold@gmx.net>
2018-05-27 21:32:16 +0200

    renamed uid to componentId

--- src/components/content/simpleWebComponent/SimpleComponent.ts
+++ src/components/content/simpleWebComponent/SimpleComponent.ts
@@ -1,40 +1,41 @@
 import {hyper} from 'hyperhtml/esm';
 import {BiotopeReduxStoreConnector} from "../../core/BiotopeReduxStoreConnector";
-import {createInitialSimpleComponentState, SimpleComponentState} from './SimpleComponentState';
+import {SimpleComponentState, createInitialSimpleComponentState} from './SimpleComponentState';
 import SimpleComponentReducerCreator from './SimpleComponent.reducer';
 import {countUp} from './SimpleComponent.actions';
 
 export class SimpleComponent extends HTMLElement {
 	static componentName: string = 'simple-component';
 	private html: any;
-	private uid: string;
+	private componentId: string;
 	private storeConnector: BiotopeReduxStoreConnector<SimpleComponentState>;
 
 	constructor() {
 		super();
 		this.html = hyper.bind(this);
+		console.log('test');
 	}
-
+ 
 	connectedCallback() {
 		this.storeConnector = new BiotopeReduxStoreConnector({
 			store: window['biotope'].store,
-			componentId: this.uid,
+			componentId: this.componentId,
 			reducerCreator: SimpleComponentReducerCreator,
 			triggerOnStateChange: (state: SimpleComponentState, lastState: SimpleComponentState) => this.onStateChange(state, lastState)
 		});
-		this.uid = this.storeConnector.getComponentId();
+		this.componentId = this.storeConnector.getComponentId();
 
 		// TODO add helpers for initial state injection
 		// JSON-LD
 		// Attributes
-		// Rehydrate
-
+		// Rehydrate 
+ 
 		this.render(createInitialSimpleComponentState());
 	}
 
 	countUp() {
 		console.log('dispatching COUNT_UP reducer');
-		this.storeConnector.dispatch(countUp(this.uid));
+		this.storeConnector.dispatch(countUp(this.componentId));
 	}
 
 	onStateChange(state: SimpleComponentState = {counter: 0}, lastState: SimpleComponentState) {
