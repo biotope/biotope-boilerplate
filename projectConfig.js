7c5432e26a286b6a53734a3ee9ef9b3bf2f506de
Filipe Martins <filipe.martins@virtual-identity.com>
2018-09-24 14:46:43 +0100

    Replace jquery.debouncedresize package

--- projectConfig.js
+++ projectConfig.js
@@ -7,6 +7,7 @@ module.exports = {
 			'jquery': 'dist/jquery.js',
 			'jquery-migrate': 'dist/jquery-migrate.js',
 			'enquire.js': 'dist/enquire.js',
+			'jquery-debouncedresize': 'js/jquery.debouncedresize.js',
 			'jquery-debouncedwidth': 'js/jquery.debouncedwidth.js',
 			'sanitize.css': 'sanitize.css',
 			'handlebars': 'dist/handlebars.runtime.js'
