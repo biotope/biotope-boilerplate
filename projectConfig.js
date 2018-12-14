c862ca23c69b89d7e53a0cd9afd8657093edb147
Jurek Barth <jurek.barth@virtual-identity.com>
2018-12-14 10:16:14 +0100

    add polyfills

--- projectConfig.js
+++ projectConfig.js
@@ -1,10 +1,27 @@
 module.exports = {
-	project: 'VI Frontend Framework 5',
+	project: 'Biotope V5',
 	global: {
+		tasks: {
+			linting: true
+		},
 		externalResources: {
 			'vi-css-helper': ['helper.css', 'print.css'],
 			'sanitize.css': 'sanitize.css',
-			'handlebars': 'dist/handlebars.runtime.js'
+			'handlebars': 'dist/handlebars.runtime.js',
+			'@webcomponents/webcomponentsjs': [
+				'custom-elements-es5-adapter.js'
+			]
 		}
+	},
+	uglify: {
+		ignoreList: [
+			'src/resources/js/polyfills/object-assign.polyfill.js',
+			'resources/js/vendor/custom-elements-es5-adapter.js',
+			'resources/js/polyfills/webcomponents-loader.js',
+			'resources/js/polyfills/bundles/webcomponents-ce.js',
+			'resources/js/polyfills/bundles/webcomponents-sd-ce-pf.js',
+			'resources/js/polyfills/bundles/webcomponents-sd-ce.js',
+			'resources/js/polyfills/bundles/webcomponents-sd.js'
+		]
 	}
 };
