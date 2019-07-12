68e0cbf6374ef369ab1c0bfa53e07eb1e6b8fec6
Timo Mayer <timo.mayer@virtual-identity.com>
2019-07-12 23:22:50 +0200

    cleanup main.js, remove default handlers for loader

--- src/resources/ts/main.ts
+++ src/resources/ts/main.ts
@@ -12,36 +12,11 @@ const vm = require('vm');
 
     const setupResourceLoader = (callback: Function) => {
 
-        const cssHandler = {
-            match: (options) => options.resource.path.indexOf('.css') > -1,
-            handle: (options) => {
-                const style = document.createElement('link');
-                style.rel = 'stylesheet';
-                style.href = options.resource.path;
-                document.body.appendChild(style);
-            }
-        }
-
-        const jsHandler = {
-            match: (options) => options.resource.path.indexOf('.js') > -1,
-            handle: (options) => {
-                const script = document.createElement('script');
-                script.src = options.resource.path;
-                script.async = true;
-                document.body.appendChild(script);
-                script.onload = () => callback();
-            }
-        }
-
         return new ResourceLoader({
             base: biotope.configuration.get('data.staticResourcesBase'),
             baseMap: {
-                '##content': biotope.configuration.get('data.staticResourcesContentRepoBase')
-            },
-            handler: [
-                cssHandler,
-                jsHandler
-            ]
+                '##microfrontends': biotope.configuration.get('data.staticResourcesMicroFrontends')
+            }
         });
 
     }
